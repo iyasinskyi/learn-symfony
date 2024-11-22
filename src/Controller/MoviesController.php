c6826c7c8626e168ac24eaf6729d6e223dbf1342
Ivan Yasinskyi <dev.yasinskyi@gmail.com>
2024-11-22 21:13:05 +0100

    Used view and added doctrine.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -5,15 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class MoviesController extends AbstractController
 {
+//    #[Route('/movies/{name}', name: 'movies', defaults:['name'=>null], methods: ['GET', 'HEAD'])]
+//    public function index($name): JsonResponse
+//    {
+//        return $this->json([
+//            'message' => $name,
+//            'path' => 'src/Controller/MoviesController.php',
+//        ]);
+//    }
+
+
     #[Route('/movies', name: 'movies')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+    public function index(): Response {
+        $movies = [
+            'Avengers: Endgame',
+            'Inceptions',
+            'Loki'
+        ];
+     return $this->render('index.html.twig', [
+         'title' => 'Movies title',
+         'movies' => $movies,
+     ]);
     }
 }
